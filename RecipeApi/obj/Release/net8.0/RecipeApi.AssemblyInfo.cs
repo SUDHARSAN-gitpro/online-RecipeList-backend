4a092b51cebac2be08e4bd5d897728a926fb9eeb
SUDHARSAN-gitpro <sudharsn24@gmail.com>
2025-08-08 10:13:48 +0530

    Remove .vs directory from tracking to respect .gitignore

--- RecipeApi/obj/Release/net8.0/RecipeApi.AssemblyInfo.cs
+++ RecipeApi/obj/Release/net8.0/RecipeApi.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba9e9190062205abbf6fcdac496c6e8c6d4aa807")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeApi")]
